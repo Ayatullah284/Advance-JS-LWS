f3972801cc63963efd9b303c8a159ab5b36494d7
Ayatullah <ayatullah284@gmail.com>
2025-12-27 07:20:51 +0600

    LWS Scopes

--- scopes.js
+++ scopes.js
@@ -1,1 +1,23 @@
-var x = 23;
+// git remote add origin https://github.com/Ayatullah284/Advance-JS-LWS.git
+
+
+
+
+
+// parent er dunia
+function myFunc(){
+    // child er dunia
+    var y = 10;
+    x = 50;
+    
+
+
+    console.log(`${x} from myFunc()`)
+}
+myFunc()
+
+
+
+
+console.log(x)
+console.log(window.x)  // window obj
