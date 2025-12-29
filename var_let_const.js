75e58e5d45d94829d53569cfbeae555d05cfbd5a
Ayatullah <ayatullah284@gmail.com>
2025-12-29 11:28:52 +0600

    const

--- var_let_const.js
+++ var_let_const.js
@@ -1,3 +1,25 @@
-// var_let_const 
-var name = 'Ayatullah';
-var friend = 'Mridul';
+if(true){
+    var varVariable ="This is var";
+    var varVariable ="This is var again";
+}
+console.log(varVariable);
+
+
+
+if(true){
+    let letVariable = "This is let";
+    letVariable = "This is let again"
+    console.log(letVariable);
+
+
+    // const redeclar/reassin kora jai na 
+    const nam = 'Ayatullah'
+    console.log(nam)
+
+    const constVariable = {
+        name: "JavaScript",
+        age: "25 years",
+    };
+    constVariable.name = "JS";
+    console.log(nam, constVariable)
+}
