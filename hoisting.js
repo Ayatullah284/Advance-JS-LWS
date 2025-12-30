eb88b6c8bd3a844ce36d2a3ac56322b2d4e5ca94
Ayatullah <ayatullah284@gmail.com>
2025-12-30 10:15:16 +0600

    var funtion let and const block scopes

--- hoisting.js
+++ hoisting.js
@@ -7,7 +7,26 @@ a = "Bangladesh"
 console.log(a) */
 
 
+/*
 console.log(a)
 
+var a = "Bangladesh"; */
 
-var a = "Bangladesh";
+
+
+
+
+let LANGUAGE = 'Java';
+let language = 'JavaScript';
+
+function getLanguage(){
+    if(!language){
+        var language = LANGUAGE; //importent line
+        console.log('condition In = ', language)
+    }
+    console.log('condition Out = ', language)
+    return language;
+}
+
+
+console.log(`I love ${getLanguage()}`)
