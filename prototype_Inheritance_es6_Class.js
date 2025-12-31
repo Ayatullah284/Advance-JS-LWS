a8ffe1ba59bad8e612c1f0ce04b0b56d4cb028b4
Ayatullah <ayatullah284@gmail.com>
2025-12-31 18:19:27 +0600

    prototype inheritance

--- prototype_Inheritance_es6_Class.js
+++ prototype_Inheritance_es6_Class.js
@@ -1,4 +1,4 @@
-var f = function Person(){}
+/* var f = function Person(){}
 
 
 
@@ -15,3 +15,38 @@ var p = {};
 p.Ayatullah();
 
 
+ */
+
+
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+
+}
+
+function Cricketer(name, age, type, country){
+    Person.call(this)
+
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    this.country = country;
+}
+
+Person.prototype = {
+    eat: function(){
+        console.log(`${this.name} is eating`);
+    }
+}
+
+
+Cricketer.prototype = Object.create(Person.prototype)
+Cricketer.prototype.constructor = Cricketer;
+Cricketer.prototype.play = function(){
+    console.log(`${this.name} is playing`)
+}
+
+
+let sakib = new Cricketer("Sakib", 35, 'All rounder', "Bangladesh");
+console.log(sakib.eat())
