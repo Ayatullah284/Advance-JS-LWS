51401476ea7c5f9e3c8c3075fff44e5760a0dff8
Ayatullah <ayatullah284@gmail.com>
2025-12-31 09:32:25 +0600

    prototype

---
+++ prototype_Inheritance_es6_Class.js
@@ -0,0 +1,23 @@
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+
+}
+
+
+Person.prototype = {
+
+    eat: function(){
+        console.log(`${this.name} is eating`)
+    }
+}
+
+const sakib = new Person('Sakib', 35);
+console.log(sakib)
+
+const tamim = new Person('tamim', 36);
+console.log(tamim)
+
+
+
+
