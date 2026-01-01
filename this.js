b390c52fb790271c0a483bc072d0901fb2cbdb00
Ayatullah <ayatullah284@gmail.com>
2026-01-01 09:38:35 +0600

    this call

--- this.js
+++ this.js
@@ -266,7 +266,7 @@ tamim.printPlayerName()
  */
 
 
-var Person = function(name, age){
+/* var Person = function(name, age){
     return{
         name: name,
         age: age,
@@ -285,4 +285,24 @@ var Person = function(name, age){
 var sakib = Person('Sakib', 35);
 // console.log(typeof sakib)
 sakib.printName()
-sakib.father.printName()
+sakib.father.printName() */
+
+
+
+var printName = function(v1, v2, v3){
+    console.log(this.name, this);
+    console.log(`${this.name} is ${v1}, ${v2}, ${v3}`)
+}
+
+var sakib = {
+    name: 'Sakib',
+    age: 36
+};
+var v1 = 'Handsome';
+var v2 = 'All-rounder';
+var v3 = 'Best Player';
+
+
+
+printName.call(sakib, v1, v2, v3)
+
