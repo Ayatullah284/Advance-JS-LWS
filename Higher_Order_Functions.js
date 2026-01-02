ea2dfd0b9df6c2d2e0ffeaf4411ef244772adc09
Ayatullah <ayatullah284@gmail.com>
2026-01-02 06:58:32 +0600

    first class funtion

---
+++ Higher_Order_Functions.js
@@ -0,0 +1,30 @@
+// figher order functions:
+
+/* 
+1. fun k parametar hesabe pathano
+2. fun return 
+
+2tar ekta thakley HOF  */
+
+
+
+/* 
+function hello(){
+    console.log(`Hello world!`);
+}
+
+hello();
+
+hello.language = 'JavaScript';
+console.dir(hello) */
+
+
+var a = function hello(){
+    console.log(`Hello world`);
+};
+
+console.dir(a);
+var str = {
+    name: 'Bangladesh',
+};
+console.log(str);
