d8301b6d98efbf3298177e125dfb83244079836a
Ayatullah <ayatullah284@gmail.com>
2026-01-02 07:38:27 +0600

    HOF buiding

--- Higher_Order_Functions.js
+++ Higher_Order_Functions.js
@@ -29,7 +29,7 @@ var str = {
 };
 console.log(str); */
 
-
+/* 
 function hello(){
     return function(){
         console.log(`Hello world!`);
@@ -39,3 +39,74 @@ function hello(){
 
 
 hello()()
+
+
+ */
+
+
+
+
+
+
+// Without HOF 
+/* var numbers = [1, 2, 3];
+
+
+var res = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    var mul = numbers[i] * 2;
+    res.push(mul);
+    
+}
+
+console.log(res);
+ */
+
+
+
+
+
+
+/* // with HOF
+var numbers = [1, 2, 3];
+
+// var result = numbers.map(function(number){
+//     return number * 2;
+
+    
+// })
+var result = numbers.map(number => number * 2);
+
+
+console.log(result) */
+
+
+
+
+
+
+var players = [
+    {
+        name: 'Sakib',
+        avg: 38.23
+    },
+    {
+        name: 'Tamim',
+        avg: 36.74
+    },
+    {
+        name: 'Mushfiq',
+        avg: 36.78
+    },
+    {
+        name: 'Mahmudullah',
+        avg: 37.12
+    }
+];
+
+var result = players.filter(player => player.avg < 38);
+
+console.log(result)
+
+
