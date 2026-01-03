e4498d3cdbc2280e99d6d9d40185c0ce418de3bd
Ayatullah <ayatullah284@gmail.com>
2026-01-03 15:35:04 +0600

    simple example

---
+++ Primitive_Reference.js
@@ -0,0 +1,24 @@
+/* var a = 5;
+var b = a;
+a = 6; */
+
+
+
+
+/* 
+var a = {
+    name: 'Sakib'
+}
+var b = a;
+a.name = 'Tamim';
+ */
+
+
+
+var a = ['Sakib', 'Tamim'];
+var b = a;
+
+a.push('Mahmudullah');
+
+console.log(a);
+console.log(b);
