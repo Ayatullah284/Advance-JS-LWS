1decebdd4f74f4f3b0b533b1f40dea670c5ff4f9
Ayatullah <ayatullah284@gmail.com>
2026-01-03 14:35:54 +0600

    promise event loop

--- Event_Loop.js
+++ Event_Loop.js
@@ -9,6 +9,7 @@ Event Loop
 
 
 
+// Async Timer: 
 /* const second = () => console.log('I am second');
 const third = () => console.log('I am third');
 
@@ -25,8 +26,8 @@ first(); */
 
 
 
-
-function process(num){
+// Sync blocking code: 
+/* function process(num){
     // process heavy sync task:
 
     console.log(num);
@@ -35,4 +36,36 @@ function process(num){
 
 [1, 2, 3, 4, 5].forEach(i => {
     process(i);
-})
+})  */
+
+
+
+// Promise a event loop:
+const second = () => console.log('second');
+const third = () => console.log('third');
+
+const first = () => {
+    console.log('first');
+    setTimeout(second, 0);
+
+    new Promise((resolve, reject) => {
+        resolve('I am rigth after third, before second');
+    }).then(resolve => console.log(resolve));
+    third();
+
+}
+
+
+
+first();
+
+
+
+// Microtask Queue 
+// Callback Queue
+
+// promise chole jai Microtask queue te 
+
+
+
+
