038935a29ae1d1b584afdc7cc6654edaeb1ec661
Ayatullah <ayatullah284@gmail.com>
2026-01-04 14:13:57 +0600

    default Object value

---
+++ Practicess_1.js
@@ -0,0 +1,46 @@
+// 1. varriable nameing
+
+
+
+
+
+
+// Default Object Value 
+// bad
+/* 
+const shapeConfig = {
+    type: 'circle',
+    width: 150,
+    height: null
+}
+
+
+function createShape(config){
+    config.type = config.type || 'circle';
+    config.width = config.width || 300;
+    config.height = config.height || 300;
+}
+
+
+
+createShape(shapeConfig);
+ */
+
+
+
+//Good 
+const shapeConfig = {
+    type: 'circle',
+    width: 150
+};
+
+function createShape(config){
+    config = Object.assign(
+        {
+            type: 'circle',
+            width: 300,
+            height: 300,
+        },
+        config
+    );
+}
