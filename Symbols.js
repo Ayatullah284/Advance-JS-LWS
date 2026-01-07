8731cb4676b792ca45a711ad1e8b632fd16c8b73
Ayatullah <ayatullah284@gmail.com>
2026-01-07 16:46:19 +0600

    Features of Symbol

--- Symbols.js
+++ Symbols.js
@@ -22,10 +22,25 @@ console.log(name); // Symbol is not a constructor */
 
 // protita symbol alada uniqe id 
 
-var nam = Symbol();
+/* var nam = Symbol();
 console.log(nam); 
 
 
 var symbl1 = Symbol.for('I am Symbol 1');
 var symbl2 = Symbol.for('I am Symbol 1');
-console.log(symbl1 == symbl2, symbl1 === symbl2) // true
+console.log(symbl1 == symbl2, symbl1 === symbl2) // true */
+
+
+
+var cricket = Symbol.for('This is Cricket Symbol.');
+var test = 'testetstestes';
+
+var object = {
+    name: 'Sakib',
+    [cricket]: 'Bangladesh',
+    [test]: 'sghdgsdgsd',
+}
+
+
+console.log(object);
+
