e2d3f2208cbed9b360f257f58751f77934957187
Ayatullah <ayatullah284@gmail.com>
2026-01-07 15:42:31 +0600

    Symbl on fire

---
+++ Symbols.js
@@ -0,0 +1,15 @@
+// Symboles in JavaScript:
+
+// Symbol is primitive data type 
+
+
+
+var symbl1 = Symbol('I am Symbol 1');
+var symbl2 = Symbol('I am Symbol 2');
+var symbl3 = Symbol();
+var symbl4 = Symbol();
+
+
+console.log(symbl1 == symbl2, symbl1 === symbl2) // false
+console.log(symbl1);
+console.log(symbl2);
