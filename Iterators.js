7302e16834acfdd8ff746d3b4823126ec91a96c6
Ayatullah <ayatullah284@gmail.com>
2026-01-09 10:46:46 +0600

    iterable and iterator

---
+++ Iterators.js
@@ -0,0 +1,32 @@
+let array = ['A', 'B', 'C'];
+let name = 'JavaScript';
+
+/* for(let i = 0; i < array.length; i++){
+    console.log(array[i]);
+} */
+/* for(let element of array){
+    console.log(element);
+} */
+/* for(let element of name){
+    console.log(element);
+} */
+console.dir(array);
+
+
+
+let object = {
+    cowboy: 'A',
+    nerd: 'B',
+    smiling: 'C',
+};
+/* let entries = Object.entries(object);
+
+for(let i = 0; i < entries.length; i++){
+
+    console.log(entries[i][1]);
+} */
+
+/*     for(let element of object){
+        console.log(element)
+    } */
+console.dir(object);
