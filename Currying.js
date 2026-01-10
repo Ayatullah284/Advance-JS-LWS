373d92de7d36eb58e987023a33b7a823c53f74f6
Ayatullah <ayatullah284@gmail.com>
2026-01-10 07:49:37 +0600

    currying & partial function

--- Currying.js
+++ Currying.js
@@ -1,5 +1,5 @@
 //  Haskell Curry 
-
+/* 
 function multiply(a, b, c){
     return a * b * c;
 }
@@ -24,5 +24,26 @@ let step2 = step1(6);
 let step3 = step2(7);
 
 console.log(step3);
+ */
 
 
+
+function discount(discount){
+    return (price)=>{
+        return price - price * discount;
+    };
+}
+
+let tenPercentDiscount = discount(0.1); // partial function
+let twentyPercentDiscount = discount(0.2);
+
+let customer1D = tenPercentDiscount(600);
+let customer2D = tenPercentDiscount(700);
+let customer3D = tenPercentDiscount(800);
+let customer4D = twentyPercentDiscount(1200);
+
+console.log(customer1D, customer2D, customer3D, customer4D);
+
+
+
+// Partial function concept:
