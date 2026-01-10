f871abcaa75a03345804d50138978b55be3d4fe0
Ayatullah <ayatullah284@gmail.com>
2026-01-10 07:22:20 +0600

    currying

---
+++ Currying.js
@@ -0,0 +1,22 @@
+//  Haskell Curry 
+
+function multiply(a, b, c){
+    return a * b * c;
+}
+
+console.log(multiply(5, 6, 7));
+
+
+
+function curriedMultiply(a){
+    return function (b){
+        return function (c){
+            return a * b * c;
+        }
+    }
+}
+
+
+console.log(curriedMultiply(5)(6)(7));
+
+
