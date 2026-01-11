f504d4b50644c38a8e7bca9084e643b950e55b33
Ayatullah <ayatullah284@gmail.com>
2026-01-11 16:59:32 +0600

    event bubbling

---
+++ Event_Propagation.js
@@ -0,0 +1,14 @@
+// Bubbling vs Capturing 
+
+
+const parent = document.getElementById('parent');
+const form = document.getElementById('form');
+const button = document.getElementById('button');
+
+parent.addEventListener('click', listener);
+form.addEventListener('click', listener);
+button.addEventListener('click', listener);
+
+function listener(event){
+    console.log(event.currentTarget);
+}
