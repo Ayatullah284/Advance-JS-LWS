9c30d2be1674eaf1f75455c57885fb1786b93c9e
Ayatullah <ayatullah284@gmail.com>
2026-01-11 09:36:11 +0600

    Intersaction observer API done

--- Intersaction_observer_API.js
+++ Intersaction_observer_API.js
@@ -1,4 +1,4 @@
-const circle = document.getElementById('circle');
+/* const circle = document.getElementById('circle');
 
 
 const observer = new IntersectionObserver((items)=>{
@@ -17,7 +17,25 @@ const observer = new IntersectionObserver((items)=>{
 
 
 observer.observe(circle);
+ */
 
+
+
+const squares = document.querySelectorAll('div');
+
+const observer = new IntersectionObserver((squares)=>{
+    squares.forEach(square => {
+        if(square.isIntersecting){
+            square.target.classList.add('visible');
+        }else{
+            square.target.classList.remove('visible');
+        }
+    });
+});
+
+
+
+squares.forEach(square => observer.observe(square));
 
 
 
