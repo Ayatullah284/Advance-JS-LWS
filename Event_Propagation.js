cf302375b4411aa34fcd553377e13789e52bc33d
Ayatullah <ayatullah284@gmail.com>
2026-01-11 17:07:09 +0600

    event capturing

--- Event_Propagation.js
+++ Event_Propagation.js
@@ -5,10 +5,14 @@ const parent = document.getElementById('parent');
 const form = document.getElementById('form');
 const button = document.getElementById('button');
 
-parent.addEventListener('click', listener);
+parent.addEventListener('click', listener, {
+    // event capturing or trickling
+    capture: true
+});
 form.addEventListener('click', listener);
 button.addEventListener('click', listener);
 
 function listener(event){
-    console.log(event.currentTarget);
+    // console.log(event.currentTarget);
+    console.log(this);
 }
